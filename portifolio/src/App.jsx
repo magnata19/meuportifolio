36bb981fd9fa8ca1780a0a5c1e5bcd88637cfca0
Davidson <dehpacifico@gmail.com>
2023-11-11 00:15:38 -0300

    adicionando backgound 3d(passivo de mudanca)

--- portifolio/src/App.jsx
+++ portifolio/src/App.jsx
@@ -1,25 +1,35 @@
+import { useEffect } from "react";
+import "./App.css";
+import Home from "./Pages/Home";
+import SobreMim from "./Pages/SobreMim";
+import Navbar from "./components/Navbar";
 
-import './App.css'
-import Home from './Pages/Home'
-import SobreMim from './Pages/SobreMim'
-import Navbar from './components/Navbar'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import NET from "vanta/src/vanta.net";
 
 function App() {
-  
+  useEffect(() => {
+    NET({
+      el: "#animation-bg",
+      mouseControls: true,
+      touchControls: true,
+    });
+  }, []);
 
   return (
     <>
-      <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/sobremim' element={<SobreMim />} />
-      </Routes>
-      </BrowserRouter>
+      <div className="animation-bg" id="animation-bg">
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/sobremim" element={<SobreMim />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
